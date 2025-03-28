a6ea920f044d6452c743a1aae91e0d987a325ccb
sdmserg <sdmserg2021@gmail.com>
2025-03-29 02:57:21 +0300

    Модель данных - Post. [#285211 #537855]. Добавлен файл Post.

---
+++ src/test/java/ru/job4j/grabber/model/Post.java
@@ -0,0 +1,109 @@
+package ru.job4j.grabber.model;
+
+import java.util.Objects;
+
+public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private Long time;
+
+    public Post() {
+
+    }
+
+    public Post(String title, String link,
+                String description, Long time) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.time = time;
+    }
+
+    public Post(Long id, String title,
+                String link, String description,
+                Long time) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return  Objects.equals(title, post.title)
+                && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", time=" + time
+                + '}';
+    }
+}
