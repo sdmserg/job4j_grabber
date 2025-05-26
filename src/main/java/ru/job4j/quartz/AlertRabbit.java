47ecfd68ff42054a9075885afbfb21f4951a948b
sdmserg <sdmserg2021@gmail.com>
2025-05-27 01:24:13 +0300

    1. Quartz [#175122]. Добавление классов AlertRabbit, файлов log4j.properties, rabbit.properties. pom.xml.

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,57 @@
+package ru.job4j.quartz;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        Properties properties = loadProperties();
+        int interval;
+        try {
+            interval = Integer.parseInt(properties.getProperty("rabbit.interval"));
+        } catch (NumberFormatException ex) {
+            throw new IllegalStateException("Property 'rabbit.interval' must be integer", ex);
+        }
+        try {
+            Scheduler sched = StdSchedulerFactory.getDefaultScheduler();
+            sched.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            sched.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static Properties loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            properties.load(input);
+        } catch (IOException ex) {
+            throw new IllegalStateException("Failed to load properties", ex);
+        }
+        return properties;
+    }
+
+    public static class Rabbit implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
