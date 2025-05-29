58ffedc0e4f0b627f18d807366be5ac96294dffa
sdmserg <sdmserg2021@gmail.com>
2025-05-29 03:14:40 +0300

    1. Quartz [#175122]. Изменене класса AlertRabbit private loadProperties.

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,14 +1,13 @@
 package ru.job4j.quartz;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
     public static void main(String[] args) {
@@ -22,7 +21,10 @@ public class AlertRabbit {
         try {
             Scheduler sched = StdSchedulerFactory.getDefaultScheduler();
             sched.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData("param1", "Hello, Rabbit!")
+                    .usingJobData("param2", 42)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(interval)
                     .repeatForever();
@@ -54,7 +56,9 @@ public class AlertRabbit {
 
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+            String param1 = context.getJobDetail().getJobDataMap().getString("param1");
+            int param2 = context.getJobDetail().getJobDataMap().getInt("param2");
+            System.out.println("Rabbit runs here with param1: " + param1 + " and param2: " + param2);
         }
     }
 }
